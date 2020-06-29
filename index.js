6e9e93e8bde54e4dd815d3067ffe14fd31768512
Marc Espín Sanz <mespinsanz@gmail.com>
2020-06-30 01:39:45 +0200

    :sparkles: Bug fixes and improvements

--- index.js
+++ index.js
@@ -1,69 +1,74 @@
-const DiscordRPC = require("discord-rpc");
+const DiscordRPC = require('discord-rpc')
+const path = require('path')
 
 function entry({ RunningConfig, Notification, StaticConfig }) {
+     const { isDev } = RunningConfig.data
+     //if(isDev) return
+     const clientId = '720362061053558805'
+     DiscordRPC.register(clientId);
+     const rpc = new DiscordRPC.Client({ transport: 'ipc' })
+     let currentTab
+     const timestamp = Date.now()
+     rpc.login({ clientId }).catch(error => {
+          new Notification({
+               title: 'DiscordRP',
+               content: 'Has been impossible to connect the rich presence to your discord.',
+               buttons: [{
+                    label: 'Click here for try again',
+                    action() {
+                         rpc.login({ clientId });
+                    }
+               }]
+          })
+     })
+     rpc.on('ready', () => {
+          rpc.setActivity({
+               largeImageKey: 'applogo',
+               largeImageText: 'Graviton Editor',
+               startTimestamp: timestamp,
+               instance: false
+          })
+     });
 
-    console.log(StaticConfig.data)
-    let clientId = '720362061053558805';
-    DiscordRPC.register(clientId);
-    const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-
-    rpc.login({ clientId }).catch(error => {
-        new Notification({
-            title: 'DiscordRP',
-            content: 'Has been impossible to connect the rich presence to your discord.',
-            buttons: [{
-                label: 'Click here for try again',
-                action() {
-                    rpc.login({ clientId });
-                }
-            }]
-        })
-    })
-    let timestamp = Date.now()
-    rpc.on('ready', () => {
-        rpc.setActivity({
-            largeImageKey: "applogo",
-            largeImageText: "Graviton Editor",
-            startTimestamp: timestamp,
-            instance: false
-        })
-        new Notification({
-            title: 'DiscordRP',
-            content: 'The connection to Discord has been stablished.'
-        })
-    });
-
-    RunningConfig.on('aTabHasBeenFocused', ({ client, instance, directory, tabElement }) => {
-        if (client) {
-            let getEditingFile = directory.split("\\");
-            let getProject = tabElement.state.data.parentFolder.split("\\")
-            let file = client.do('getMode', { instance }).name;
-            switch (file) {
-            case "application/json":
-                file = "json"
-                break;
-            case "gfm":
-                file = "md"
-                break;
-            }
-            rpc.setActivity({
-                details: `Editing ${getEditingFile[getEditingFile.length - 1]}`,
-                state: `Workspace: ${getEditingFile[getProject.length - 1]}`,
-                startTimestamp: timestamp,
-                largeImageKey: file,
-                largeImageText: `Editing a ${file} file`,
-                smallImageKey: 'applogo',
-                smallImageText: 'Graviton',
-                instance: false,
-            });
-        }
-    });
-
-    StaticConfig.keyChanged('discordRP',()=>{
-        console.log('A new theme has been selected')
-    })
+     RunningConfig.on('aTabHasBeenFocused', ({ client, instance, parentFolder, directory, tabElement }) => {
+          if (client) {
+               let editingFile = path.basename(directory)
+               let workingProject = path.normalize(parentFolder)
+               let file = client.do('getMode', { instance }).name;
+               switch (file) {
+                    case 'application/json':
+                         file = 'json'
+                         break;
+                    case 'gfm':
+                         file = 'md'
+                         break;
+               }
+               rpc.setActivity({
+                    details: `Editing ${editingFile}`,
+                    state: `Workspace: ${workingProject}`,
+                    startTimestamp: timestamp,
+                    largeImageKey: file,
+                    largeImageText: `Editing a ${file} file`,
+                    smallImageKey: 'applogo',
+                    smallImageText: 'Graviton',
+                    instance: false,
+               })
+               currentTab = tabElement
+          }
+     })
+     RunningConfig.on('aTabHasBeenClosed', ({ client, instance, directory, tabElement }) => {
+          if (client && tabElement === currentTab) {
+               rpc.setActivity({
+                    largeImageKey: 'applogo',
+                    largeImageText: 'Graviton Editor',
+                    startTimestamp: timestamp,
+                    instance: false
+               })
+               currentTab = null
+          }
+     })
 }
 
 module.exports = {
-    entry
+     entry
 }
